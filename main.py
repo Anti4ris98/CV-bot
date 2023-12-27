16936eba1406d29339f59986b20cbcfa970aa1a3
Anti4ris98 <142513292+Anti4ris98@users.noreply.github.com>
2023-12-27 21:16:35 +0200

    It WORKS

--- main.py
+++ main.py
@@ -1,15 +1,119 @@
 import telebot
 import webbrowser
+from telebot import types
+import openpyxl
+from pathlib import Path
+
 
 bot = telebot.TeleBot('6896862671:AAE9GWFSnReoGNHF5Q0cvi03iPDRjqNWvM4')
 
 @bot.message_handler(commands=['start'])
 def main(message):
     if message.from_user.language_code == 'uk':
-        bot.send_message(message.chat.id, f"Доброго дня {message.from_user.first_name}{message.from_user.last_name}, Мене звати Ілля. То з чого почнемо знайомство? Напишіть слово <b>Допомога</b> і я розкажу що я могу вам розказати", parse_mode='html')
+         bot.send_message(message.chat.id, f"Доброго дня {message.from_user.first_name}{message.from_user.last_name}, Мене звати Ілля. То з чого почнемо знайомство? Напишіть слово <b>Допомога</b> і я розкажу що я могу вам розказати", parse_mode='html')
+
     elif message.from_user.language_code == 'en':
         bot.send_message(message.chat.id, f"Hello {message.from_user.first_name}{message.from_user.last_name}, my name is Illia Lobachov, let`s start our acquaintance. Just write <b>Assistance</b> and I will describe you what I can what can I tell about myself", parse_mode='html')
 
+@bot.message_handler(commands=['summary'])
+def summary(message):
+    file_path = Path(__file__).parent / "infoeng.xlsx"
+    try:
+        wb = openpyxl.load_workbook(file_path)
+        sheet = wb.worksheets[0]
+
+        for row in sheet.iter_rows(values_only=True):
+            row_data = ', '.join(map(str, row))
+            bot.send_message(message.chat.id, row_data)
+    except Exception as e:
+        bot.send_message(message.chat.id, f"An error occurred: {str(e)}")
+
+@bot.message_handler(commands=['самарі'])
+def summary(message):
+    file_path = Path(__file__).parent / "infoua.xlsx"
+    try:
+        wb = openpyxl.load_workbook(file_path)
+        sheet = wb.worksheets[0]
+        for row in sheet.iter_rows(values_only=True):
+            row_data = ', '.join(map(str, row))
+            bot.send_message(message.chat.id, row_data)
+    except Exception as e:
+        bot.send_message(message.chat.id, f"An error occurred: {str(e)}")
+
+
+@bot.message_handler(commands=['academic'])
+def summary(message):
+    file_path = Path(__file__).parent / "infoeng.xlsx"
+    try:
+        wb = openpyxl.load_workbook(file_path)
+        sheet = wb.worksheets[1]
+        for row in sheet.iter_rows(values_only=True):
+            row_data = ', '.join(map(str, row))
+            bot.send_message(message.chat.id, row_data)
+    except Exception as e:
+        bot.send_message(message.chat.id, f"An error occurred: {str(e)}")
+
+@bot.message_handler(commands=['навчання'])
+def summary(message):
+    file_path = Path(__file__).parent / "infoua.xlsx"
+    try:
+        wb = openpyxl.load_workbook(file_path)
+        sheet = wb.worksheets[1]
+        for row in sheet.iter_rows(values_only=True):
+            row_data = ', '.join(map(str, row))
+            bot.send_message(message.chat.id, row_data)
+    except Exception as e:
+        bot.send_message(message.chat.id, f"An error occurred: {str(e)}")
+
+
+@bot.message_handler(commands=['experience'])
+def summary(message):
+    file_path = Path(__file__).parent / "infoeng.xlsx"
+    try:
+        wb = openpyxl.load_workbook(file_path)
+        sheet = wb.worksheets[2]
+        for row in sheet.iter_rows(values_only=True):
+            row_data = ', '.join(map(str, row))
+            bot.send_message(message.chat.id, row_data)
+    except Exception as e:
+        bot.send_message(message.chat.id, f"An error occurred: {str(e)}")
+
+@bot.message_handler(commands=['досвід'])
+def summary(message):
+    file_path = Path(__file__).parent / "infoua.xlsx"
+    try:
+        wb = openpyxl.load_workbook(file_path)
+        sheet = wb.worksheets[2]
+        for row in sheet.iter_rows(values_only=True):
+            row_data = ', '.join(map(str, row))
+            bot.send_message(message.chat.id, row_data)
+    except Exception as e:
+        bot.send_message(message.chat.id, f"An error occurred: {str(e)}")
+
+
+@bot.message_handler(commands=['contact'])
+def summary(message):
+    file_path = Path(__file__).parent / "infoeng.xlsx"
+    try:
+        wb = openpyxl.load_workbook(file_path)
+        sheet = wb.worksheets[3]
+        for row in sheet.iter_rows(values_only=True):
+            row_data = ', '.join(map(str, row))
+            bot.send_message(message.chat.id, row_data)
+    except Exception as e:
+        bot.send_message(message.chat.id, f"An error occurred: {str(e)}")
+
+@bot.message_handler(commands=['контакти'])
+def summary(message):
+    file_path = Path(__file__).parent / "infoua.xlsx"
+    try:
+        wb = openpyxl.load_workbook(file_path)
+        sheet = wb.worksheets[3]
+        for row in sheet.iter_rows(values_only=True):
+            row_data = ', '.join(map(str, row))
+            bot.send_message(message.chat.id, row_data)
+    except Exception as e:
+        bot.send_message(message.chat.id, f"An error occurred: {str(e)}")
 
 @bot.message_handler(commands=['info'])
 def main(message):
@@ -31,16 +135,17 @@ def site(message):
 def help(message):
     if message.text.lower() == 'допомога':
         bot.send_message(message.chat.id, '<b>За допомогою цих фраз я...</b>', parse_mode='html')
-        bot.send_message(message.chat.id, '<em>/summary</em> - розповім що вмію', parse_mode='html')
-        bot.send_message(message.chat.id, '<em>/academic</em> - розповім де вчився', parse_mode='html')
-        bot.send_message(message.chat.id, '<em>/expirience</em> - розповім свою професійну історію', parse_mode='html')
-        bot.send_message(message.chat.id, '<em>/contact</em> - як зі мною звязатись', parse_mode='html')
-        bot.send_message(message.chat.id, '<em>/export</em> - вишлю ці данні вам на пошту', parse_mode='html')
+        bot.send_message(message.chat.id, '<em>/самарі</em> - розповім що вмію', parse_mode='html')
+        bot.send_message(message.chat.id, '<em>/навчання</em> - розповім де вчився', parse_mode='html')
+        bot.send_message(message.chat.id, '<em>/досвід</em> - розповім свою професійну історію', parse_mode='html')
+        bot.send_message(message.chat.id, '<em>/контакти</em> - як зі мною звязатись', parse_mode='html')
+        bot.send_message(message.chat.id, '<em>/скачати</em> - вишлю ці данні вам на пошту', parse_mode='html')
+    
     elif message.text.lower() == 'assistance':
         bot.send_message(message.chat.id, '<b>By using this comands I will ...</b>', parse_mode='html')
         bot.send_message(message.chat.id, '<em>/summary</em> - describe my abbilities', parse_mode='html')
         bot.send_message(message.chat.id, '<em>/academic</em> - tell about my academic history', parse_mode='html')
-        bot.send_message(message.chat.id, '<em>/expirience</em> - tell you my сareer пrowth', parse_mode='html')
+        bot.send_message(message.chat.id, '<em>/experience</em> - tell you my сareer пrowth', parse_mode='html')
         bot.send_message(message.chat.id, '<em>/contact</em> - how to contack with me', parse_mode='html')
         bot.send_message(message.chat.id, '<em>/export</em> - send CV by email', parse_mode='html')
                  
